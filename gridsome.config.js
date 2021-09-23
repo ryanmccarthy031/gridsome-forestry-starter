7796ee12eba09d9b729d7d6ab1f79d6cc9829d68
Ryan McCarthy <ryanmccarthy031@gmail.com>
2021-09-23 03:57:59 +0000

    Revert "Remove extra dir layer"
    This reverts commit 3b44243d913711f6389fbfb7b78caf25791f83cd.

--- gridsome.config.js
+++ gridsome.config.js
@@ -11,9 +11,10 @@ module.exports = {
     {
       use: "@gridsome/source-filesystem",
       options: {
-        path: "pages/*.md",
+        path: "pages/**/*.md",
         typeName: "MainPage",
         resolveAbsolutePaths: true,
+        component: './src/templates/Page.vue',
         remark: {
           externalLinksTarget: "_blank",
           externalLinksRel: ["nofollow", "noopener", "noreferrer"]
