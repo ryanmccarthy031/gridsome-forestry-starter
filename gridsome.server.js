bd0addcdcbc284bc4304a69112e96956a7d6cc49
Ryan McCarthy <ryanmccarthy031@gmail.com>
2021-10-13 14:59:47 +0000

    Pages api?

---
+++ gridsome.server.js
@@ -0,0 +1,30 @@
+//gridsome.server.js
+module.exports = function (api) {
+    api.createPages(async ({ graphql, createPage }) => {
+      const { data } = await graphql(`{
+        allMainPage {
+          edges {
+            node {
+                id
+                path
+                title
+                locale
+            }
+          }
+        }
+      }`)
+  
+      data.allMainPage.edges.forEach(({ node }) => {
+        createPage({
+          path: node.path,
+          component: './src/templates/MainPage.vue',
+          context: {
+            id: node.id,
+            locale: node.locale,
+            title: node.title,
+            path: node.path,
+          }
+        })
+      })
+    })
+  }
