d1fb5d7bcab1c0edd659bdd42cad6c3b6b6386db
Ryan McCarthy <ryanmccarthy031@gmail.com>
2021-10-13 15:24:18 +0000

    Add query again

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,6 +5,20 @@
     </div>
   </Layout>
 </template>
+<page-query>
+query MainPage ($locale: String) {
+  pages: allMainPage(filter: { locale: { eq: $locale } }, sort: [{ by: "priority", order: DESC }]) {
+    edges {
+        node {
+            id
+            path
+            title
+            locale
+        }
+    }
+  }
+}
+</page-query>
 <script>
 import Hero from "@/components/Hero"
 
