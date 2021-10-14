ff99c13708c97915daf6c876c8b4915e0eaf95b5
Ryan McCarthy <ryanmccarthy031@gmail.com>
2021-10-14 03:16:42 +0000

    Hero styles

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,9 +1,13 @@
 <template>
-    <div>
-        <h1 v-html="settings.hero_title" />
-        <h2 
+    <div class="max-w-full prose grid grid-cols-12">
+        <h1
+          class="col-span-8 col-start-3"
+          v-html="settings.hero_title" />
+        <h2
+          class="col-span-8 col-start-3"
           v-if="$i18n.locale.toString()==='en-US'" v-html="settings.english_subtitle" />
         <h2 
+          class="col-span-8 col-start-3"
           v-if="$i18n.locale.toString()==='es-ES'" v-html="settings.subtítulo_en_español" />
     </div>
 </template>
